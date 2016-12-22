6fd3c93e8e76742ba4fb49b9da18eb0e3481152f
maxgrok <maxgrok2011@gmail.com>
2016-12-21 20:22:56 -0500

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+     return programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     return programmer_hash[:dennis_ritchie][:languages].join("")
 end
 
 def adding_matz
@@ -79,9 +81,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+     programmer_hash[:yukihiro_matsumoto]={known_for: "Ruby", languages: "LISP, C"} 
+     return programmer_hash
 end
+adding_matz
 
 def changing_alan
 	programmer_hash = 
@@ -101,25 +104,29 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     return programmer_hash
 end
 
 def adding_to_dennis
 	programmer_hash = 
  		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-
+      :grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+      },
+      :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C"]
+      }
+    }
+  dennis_ritchie = programmer_hash[:dennis_ritchie]
+  languages = programmer_hash[:dennis_ritchie][:languages]
+  languages << 'Assembly'
+  return programmer_hash
 end
+adding_to_dennis
