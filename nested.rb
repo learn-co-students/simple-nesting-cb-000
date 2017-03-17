63690f70cfaf98036ecaeff3561664fb3d770723
youssefmys <mohamed.saleh@vet.svu.edu.eg>
2017-03-17 15:04:00 +0200

    Done.

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+        programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +36,8 @@ def alan_kay_is_known_for
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
+                }
+        programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -54,7 +55,9 @@ def dennis_ritchies_language
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
+                }
+
+        programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
@@ -78,8 +81,14 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
+                }
+        
+        programmer_hash[:yukihiro_matsumoto] = {
+          :known_for => "Ruby",
+          :languages => ["LISP ,C"]
+        }
 
+        programmer_hash
     
 end
 
@@ -99,10 +108,10 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
+
      alans_new_info = "GUI"
-     
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
 end
 
 def adding_to_dennis
@@ -120,6 +129,10 @@ def adding_to_dennis
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
-
+                }
+        
+        programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+        programmer_hash
 end
+
+puts adding_to_dennis
